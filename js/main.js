25cbdac3cb29d42dfc2d5a199f85fd833ed8179a
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-04-23 16:47:15 +0200

    Added: M4 - code for previous img

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ document.getElementById("nextB").addEventListener("click", function(){
     selectedImg.classList.remove("active");
     nextImg.classList.add("active");
 });
+
+document.getElementById("prevB").addEventListener("click", function(){
+    
+    const selectedImg = document.querySelector(".active");
+    let nextImg = selectedImg.previousElementSibling;
+    
+    selectedImg.classList.remove("active");
+    nextImg.classList.add("active");
+});
