a489df45108a9d8dadc622778297326c4803560a
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-04-23 17:14:50 +0200

    Added: bonus 1 (previous to be fixed)

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
+//NEXT BUTTON
 document.getElementById("nextB").addEventListener("click", function(){
     
     const selectedImg = document.querySelector(".active");
     let nextImg = selectedImg.nextElementSibling;
     
     selectedImg.classList.remove("active");
+
+    if(!nextImg.classList.contains("set-img")){
+        nextImg= document.querySelector(".slide:first-child"); 
+        nextImg.classList.add("active");
+    }
+    else{
     nextImg.classList.add("active");
+    }
 });
 
+//PREVIOUS BUTTON
 document.getElementById("prevB").addEventListener("click", function(){
     
     const selectedImg = document.querySelector(".active");
     let nextImg = selectedImg.previousElementSibling;
     
     selectedImg.classList.remove("active");
+
+    if(!nextImg.classList.contains("set-img")){
+        nextImg= document.querySelector(".slide:last-child"); 
+        nextImg.classList.add("active");
+    }
+    else{
     nextImg.classList.add("active");
+    }
 });
