af060bdb71f13afae10a87a75b2f64c9658987ec
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-04-23 16:44:37 +0200

    Added: M3 - previous code in 'nextB' button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-const selectedImg = document.querySelector(".active");
-let nextImg = selectedImg.nextElementSibling;
-
-selectedImg.classList.remove("active");
-nextImg.classList.add("active");
+document.getElementById("nextB").addEventListener("click", function(){
+    
+    const selectedImg = document.querySelector(".active");
+    let nextImg = selectedImg.nextElementSibling;
+    
+    selectedImg.classList.remove("active");
+    nextImg.classList.add("active");
+});
